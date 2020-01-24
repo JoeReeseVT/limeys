e85605e2a421094226d3562fe5bbcb4c96069f65
colemtaylor <60193967+colemtaylor@users.noreply.github.com>
2020-01-24 14:11:45 -0500

    Update 01A.ino

--- 01A.ino
+++ 01A.ino
@@ -1,11 +1,16 @@
+// port constant declorations
+const uint8_t NONE     = 1; // allOffExcept(NONE) to turn all leds off
+const uint8_t REDLED   = 2; // red led port
+const uint8_t GRNLED   = 3; // green led port
+const uint8_t BLUELED  = 4; // blue led port
+const uint8_t ORNGLED  = 5; // orange led port
+const uint8_t SWCH0    = 6; // switch port
+const uint8_t BTN[3]   = {7, 8, 9}; // array of button ports
 
-const uint8_t REDLED   = 2;
-const uint8_t GREENLED = 3;
-const uint8_t BLUELED  = 4;
-const uint8_t ORANGE   = 5;
-const uint8_t SWCH0    = 6;
-const uint8_t BTN[3]   = {7, 8, 9}; 
-int brightness = 0;
+// for diagnostic mode
+int numProbs = 5;
+
+// state variable declorations
 int state = 0;
 const int OFF  = 0;
 const int STBY = 1;
@@ -17,42 +22,17 @@ const unsigned long DEBOUNCE_DELAY = 5; // milliseconds
 unsigned long debounceTimer = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   for (int i = 2; i <= 5; i++)
     pinMode(i, OUTPUT); // setup LED pins to output
-  for (int k = 6; k <= 7; k++)
+  for (int k = 6; k <= 9; k++)
     pinMode(k, INPUT_PULLUP); // setup pins to pull up
-  allOff();
+  allOffExcept(NONE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
   stateMachine();
 }
 
-#if 0
-void switchISR_() {
-  static const unsigned long DELAY = 5; // ms
-  static unsigned long timer = 0;
-
-  static bool curState;
-  static bool prvState = false;
-  static bool output;
-
-  curState = digitalRead(pin);
-
-  if (curState ^ prvState)
-    timer = millis();
-   
-  if (millis() - timer >= DELAY)
-    output = curState;   
-  
-  prvState = curState;
-
-  return output;
-}
-#endif
 
 // State machine for part 1A
 void stateMachine() {
@@ -64,40 +44,69 @@ void stateMachine() {
         ledStby();
       }
       break;
-    // standby state blinking red led  
+    // standby state blinking red led
+    // can move to run, sleep, or off state
     case STBY: 
       if (not digitalRead(SWCH0)) {
         state = OFF;
-        allOff();
+        allOffExcept(NONE);
       } else if (not digitalRead(BTN[0])) {
         state = RUN;
         ledRun();
-      }else if (not digitalRead(BTN[1])) {
-        
+      } else if (not digitalRead(BTN[1])) {
+        state = SLP;
+        ledSlp();
       } else 
         ledStby(); 
       break;
     // run state solid green led
+    // can move to sleep, diagnostic, or off state
     case RUN:
       if (not digitalRead(SWCH0)) {
         state = OFF;
-        allOff();
-      else
-      }
+        allOffExcept(NONE);
+      } else if (not digitalRead(BTN[1])) {
+        state = SLP;
+        ledSlp();
+      } else if (not digitalRead(BTN[2])) {
+        state = DIAG;
+        ledDiag(numProbs);
+      } else
+        ledRun();
       break;
+    // sleep state breathing blue led
+    // can move to run state or off state
     case SLP:
       if (not digitalRead(SWCH0)) {
         state = OFF;
-        allOff();
-      }
+        allOffExcept(NONE);
+      } else if (not digitalRead(BTN[0])) {
+        state = RUN;
+        ledRun();
+      } else 
+        ledSlp();
+      break;
+    // diagnostic state blinks led for number of problems
+    // can only move to off state
+    case DIAG:
+      if (not digitalRead(SWCH0)) {
+        state = OFF;
+        allOffExcept(NONE);
+      } else 
+        ledDiag(numProbs);
+      break;
   }
 }
 
-void allOff() {
-  for (int i = 2; i < 5; i++)
+void allOffExcept(uint8_t pinOn) { // pinOn is the port for the led in use
+  for (int i = 2; i < pinOn; i++)
     analogWrite(i, 0);
+
+  for (int k = pinOn + 1; k < 5; k++)
+    analogWrite(k, 0);
 }
 void ledStby() {
+  allOffExcept(REDLED);
   static unsigned long timer = 0;
   static const unsigned long HALF_CYCLE = 500; // milliseconds
   static bool isOn = true;
@@ -111,6 +120,55 @@ void ledStby() {
 }
 
 void ledRun() {
-  analogWrite(REDLED, 0); // turn off green led
-  analogWrite(GREENLED, 127); // 50% duty cycle
+  allOffExcept(GRNLED);
+  analogWrite(GRNLED, 127); // 50% duty cycle
+}
+
+void ledSlp() {
+  allOffExcept(BLUELED);
+  
+  static unsigned long timer = 0;
+  static const unsigned long DELAY_TIME = 75; // milliseconds
+  static unsigned long fadeAmount = 4;
+  static uint8_t brightness = 0;
+
+  // delay
+  if (millis() - timer >= DELAY_TIME) {
+    timer = millis();
+    brightness = brightness + fadeAmount;
+    // reverse direction of count
+    if (brightness < 4 || brightness >= 127) {
+    fadeAmount = -fadeAmount;
+    }
+  }
+  
+  analogWrite(BLUELED, brightness);
+}
+
+// blinks led for "numProbs" number of times
+// then turns off for a period of time
+// then repeats
+void ledDiag(int numProbs) {
+  allOffExcept(ORNGLED);
+  static unsigned long timer[2] = {0, 0};
+  static const unsigned long BLINK = 300; // milliseconds
+  static const unsigned long SOLID = BLINK * (2 * numProbs); // milliseconds
+  static bool isOn    = true;
+  static bool isOff = true;
+  
+  // toggles if led is off or blinking
+  if (millis() - timer[0] >= SOLID) {
+    timer[0] = millis();
+    isOff = not isOff;
+  }
+  
+  if (not isOff) {
+    // toggles led in blink mode
+    if (millis() - timer[1] >= BLINK) {
+      timer[1] = millis();
+      isOn = not isOn;
+    }
+    analogWrite(ORNGLED, 127 * isOn); // 50% duty cycle
+  } else
+    analogWrite(ORNGLED, 0); // off when not blinking
 }
